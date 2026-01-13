c38c65f2a9834b54d0046a625afb72ce16194c39
perse <persele2580@163.com>
2026-01-13 22:39:56 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "docs" // GitHub Pages 默认识别 docs 目录
+  }
 })
